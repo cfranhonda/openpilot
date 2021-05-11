e2c97f3fa3ef2a0ecc2d62186863f88404a2c453
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2021-05-11 09:50:18 -0700

    Create interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -214,10 +214,10 @@ class CarInterface(CarInterfaceBase):
     
       ret.longitudinalTuning.deadzoneBP = [0., 40.]
       ret.longitudinalTuning.deadzoneV = [.0, .02] #trying to create a little more "give"
-      #ret.longitudinalTuning.kpBP = [0., 10., 40.]
-      #ret.longitudinalTuning.kpV = [1.2, 0.6, 0.2]
-      #ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
-      #ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
+      ret.longitudinalTuning.kpBP = [0., 10., 40.]
+      ret.longitudinalTuning.kpV = [1.2, 0.6, 0.2]
+      ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
+      ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
 
     elif candidate in (CAR.ACCORD, CAR.ACCORD_15, CAR.ACCORDH):
       stop_and_go = True
