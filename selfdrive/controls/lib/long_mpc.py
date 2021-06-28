845fb64720f75e4d66d847045afe1e06726b4ce2
Casey Francis <@CFranHonda>
2021-06-28 13:23:40 -0700

    Update long_mpc.py

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -15,7 +15,7 @@ TWO_BAR_DISTANCE = 1.5  # in seconds
 THREE_BAR_DISTANCE = 1.8  # in seconds
 FOUR_BAR_DISTANCE = 2.1   # in seconds
 STOPPING_DISTANCE = 2 # distance between you and lead car when you come to stop
-TR = TWO_BAR_DISTANCE  # default interval
+TR = THREE_BAR_DISTANCE  # default interval
 
 # Variables that change braking profiles
 CITY_SPEED = 22.4  # braking profile changes when below this speed based on following dynamics below [m/s]
